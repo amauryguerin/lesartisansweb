603bd2abbd02d3d759c260f9129abefd2c10e7b9
Amaury Guérin <amaury@ihaveagreen.fr>
2022-12-14 11:06:00 +0100

    update fonction alt ajout figcaption

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const docP = document.querySelectorAll( "p" );
+const docFigcaption = document.querySelectorAll( "figcaption" );
 
 document.addEventListener('keydown', keyDown);
 function keyDown(event) {   
     //alt key down
     if(event.keyCode === 18) {
         docP.forEach((e) => {
+            e.classList.add("onAltPressed");
+        });
+        docFigcaption.forEach((e) => {
             e.classList.add("onAltPressed");
         });
     }
@@ -15,6 +19,9 @@ function keyUp(event) {
     //alt key up
     if(event.keyCode === 18) {
         docP.forEach((e) => {
+            e.classList.remove("onAltPressed");
+        });
+        docFigcaption.forEach((e) => {
             e.classList.remove("onAltPressed");
         });
     }
